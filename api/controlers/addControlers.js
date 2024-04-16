5143748093f97695044f45a9822eedb1a91f3754
fevan <evan.ferron53@gmail.com>
2024-04-16 20:11:52 +0200

    :wrench: | working on controllers

--- api/controlers/addControlers.js
+++ api/controlers/addControlers.js
@@ -1,35 +1,21 @@
 const Database = require("../Database.js");
-const DB_PATH = "./clubs.db";
+const DB_PATH = "./racingGame.db";
 
-exports.addPlayer = async (req,res) => {
-    const player = req.body;
-    // TO DO : verif data format
-    const password = hashFunc.hashPassword("sha256", "base64", player.password);
+exports.addGame = async (req, res) => {
+  const game = req.body;
 
-    let err = await Database.Write(
-        DB_PATH,
-        "INSERT INTO players (nickname,email,password) VALUES (?,?,?);",
-        player.nickname,
-        player.email,
-        password
-    )
-    if (err != null) {
-        console.error(err);
-        res.json({ status: false });
-        return;
-    }
-    console.log("a player has been successfully add");
-    res.json({ status: true });
-}
-
-exports.addGame = async (req,res) => {
-    const game = req.body;
-
-    let err = await Database.Write(
-        DB_PATH,
-        "INSERT INTO history (winnerId,loserId,gamemodeId) VALUES (?,?,?);",
-        game.winnerId,
-        game.loserId,
-        game.gamemodeId
-    )
-}
+  let err = await Database.Write(
+    DB_PATH,
+    "INSERT INTO history (winnerId,loserId,gamemodeId) VALUES (?,?,?);",
+    game.winnerId,
+    game.loserId,
+    game.gamemodeId
+  );
+  if (err != null) {
+    console.error(err);
+    res.json({ status: false });
+    return;
+  }
+  console.log("a game has been successfully add");
+  res.json({ status: true });
+};
---
+++ api/controlers/authControlers.js
@@ -0,0 +1,95 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const GetControlers = require("./getControlers");
+
+dotenv.config();
+
+const SECRET_KEY = process.env.SECRET_KEY;
+const DB_PATH = process.env.DB_PATH;
+
+async function register(req, res) {
+  const player = req.body;
+  try {
+    const password = await bcrypt.hash(player.password, 10);
+    let err = await Database.Write(
+      DB_PATH,
+      "INSERT INTO players (nickname,email,password) VALUES (?,?,?);",
+      player.nickname,
+      player.email,
+      password
+    );
+    if (err != null) {
+      console.error(err);
+      res.json({ status: false });
+      return;
+    }
+    err = await initPlayerData(player.nickname);
+    if (!err) {
+      console.log("an error happend when trie to initialise data");
+      return;
+    }
+    res.status(201).send("User registered successfully");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error registering user");
+  }
+}
+
+async function login(req, res) {
+  const player = req.body;
+  // TO DO : check data format
+  const user = await Database.Read(
+    DB_PATH,
+    "SELECT playerId,password FROM players WHERE nickname=?;",
+    player.nickname
+  );
+  if (user == null) return res.status(400).send("User not found");
+  try {
+    if (await bcrypt.compare(player.password, user.password)) {
+      const rank = await Database.Read(
+        DB_PATH,
+        "SELECT name,points,ranks.name,gamemode.name FROM playersRank LEFT JOIN gamemode ON playersRank.gamemodeId = gamemode.gamemodeId LEFT JOIN ranks ON playersRank.rankId = ranks.rankId WHERE playerId = ?;",
+        user.playerId
+      );
+      const accessToken = jwt.sign({ playerId: user.playerId }, SECRET_KEY);
+      res.json({ accessToken: accessToken, rank: rank });
+    } else {
+      res.status(401).send("Invalid password");
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error logging in");
+  }
+}
+
+module.exports = {
+  register,
+  login,
+};
+
+// local function to initialise every data wich the player is related from
+// return false if the operation hasn't succed
+const initPlayerData = async (nickname) => {
+  const defaultPoints = 100;
+  const ranks = await GetControlers.getRanks();
+  const gamemodes = await GetControlers.getGamemodes();
+  const playerId = await GetControlers.getIdByNickname(nickname);
+  const silverRank = ranks.find((data) => data[1] === "silver"); // get silver id
+  if (playerId == -1 || silverRank == null) return false;
+  for (let gamemode of gamemodes) {
+    const err = await Database.Write(
+      DB_PATH,
+      "INSERT INTO playersRank(playersId,points,gamemodeId,rankId;",
+      playerId,
+      defaultPoints,
+      gamemode.gamemodeId,
+      silverRank[0]
+    );
+    if (err != null) {
+      console.error(err);
+      return false;
+    }
+  }
+  return true;
+};
