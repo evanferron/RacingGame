4ea0a719dcf08d808e2ab793e11380da37493c7a
fevan <evan.ferron53@gmail.com>
2024-04-25 14:42:31 +0200

    :zap: | improve error system in auth system

---
+++ api/utils/checkData.js
@@ -0,0 +1,69 @@
+const AuthError = require("../error/authError.js");
+
+const checkGamemodeName = (gamedemode) => {
+  switch (gamedemode) {
+    case "Dice":
+      return true;
+    case "SpeedTyping":
+      return true;
+    default:
+      return false;
+  }
+};
+
+const checkRank = (rank) => {
+  switch (rank) {
+    case "bronze":
+      return true;
+    case "silver":
+      return true;
+    case "gold":
+      return true;
+    case "diamond":
+      return true;
+    default:
+      return false;
+  }
+};
+
+const handleAuthTest = (nickname, password, email = "exemple@exemple.com") => {
+  if (!testEmail(email)) {
+    throw new AuthError(
+      "invalidEmailFormat",
+      "The format is not valid for the email : " + player.email,
+      400
+    );
+  }
+  if (!testNickname(nickname)) {
+    throw new AuthError(
+      "invalidNicknameFormat",
+      "The format is not valid for the nickname : " + player.nickname,
+      400
+    );
+  }
+  if (!testPassword(password)) {
+    throw new AuthError(
+      "invalidPasswordFormat",
+      "caracters ( [ ' ; ] ) are not allowed",
+      400
+    );
+  }
+  return "";
+};
+
+const testEmail = (email) => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+};
+const testNickname = (nickname) => {
+  return /^[a-zA-Z0-9_-]{3,16}$/.test(nickname);
+};
+
+const testPassword = (password) => {
+  return /([';])/.test(password);
+};
+
+module.exports = {
+  checkGamemodeName,
+  checkRank,
+  handleAuthTest,
+};
