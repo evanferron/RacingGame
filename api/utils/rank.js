a6b4389aa78039fc3e8e3486a3496f5f75befac6
fevan <evan.ferron53@gmail.com>
2024-04-30 11:58:40 +0200

    :bug: | fix end game situation bug

--- api/utils/rank.js
+++ api/utils/rank.js
@@ -4,12 +4,12 @@ const DB_PATH = "./racingGame.db";
 
 // !!!! NEED TO REWORK THIS (not optimal and very simplistic)
 const getNewPlayerPoints = (winnerPoints, looserPoints) => {
-  const maxPointsLoose = 50;
-  const maxPointsWin = 80;
-  const minPointsWin = 5;
-  const minPointsLoose = 4;
-  const pointsWin = 9;
-  const pointsLoose = 9;
+  let maxPointsLoose = 50;
+  let maxPointsWin = 80;
+  let minPointsWin = 5;
+  let minPointsLoose = 4;
+  let pointsWin = 9;
+  let pointsLoose = 9;
   if (looserPoints - winnerPoints > 20) {
     pointsWin = Math.round((looserPoints - winnerPoints) / 2);
     pointsLoose = Math.round((looserPoints - winnerPoints) / 2);
